3cc7d88f6f1c75c23fd8547d4535195ed94a872b
Dillon Teakell <teakelld@icloud.com>
2024-08-14 11:35:13 -0400

    Initial Commit

---
+++ NetworkCalls/NetworkCallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkCallsApp.swift
+//  NetworkCalls
+//
+//  Created by Dillon Teakell on 8/14/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkCallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
