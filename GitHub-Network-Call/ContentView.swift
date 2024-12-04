e286f9e5703b013dbbc1af94b551f60beb3c764d
Dillon Teakell <teakelld@icloud.com>
2024-12-04 12:33:22 -0500

    Initial Commit

---
+++ GitHub-Network-Call/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHub-Network-Call
+//
+//  Created by Dillon Teakell on 12/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHub-Network-Call/GitHub_Network_CallApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_Network_CallApp.swift
+//  GitHub-Network-Call
+//
+//  Created by Dillon Teakell on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_Network_CallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
